88eb0226561860dbcfff3df561bfbd4587e51c1d
mattciff5 <matteociferri.1995@gmail.com>
2023-11-22 18:14:57 +0000

    plotting meg/speech

--- collect_data.py
+++ collect_data.py
@@ -8,30 +8,12 @@ import matplotlib.pyplot as plt
 import torch 
 
 shift_value = 1e13
-duration = 3   # secondsß
+duration = 3   # seconds
 decim = 1
 n_fft = 512 
 hop_length = n_fft // 4  
 sampling_audio = 16000
 
-# maybe to remove
-def get_meg_from_raw_toRm(raw, data_meg_chunks):
-    x = raw.get_data()
-    sampling_freq = raw.info["sfreq"] 
-    tot_length_meg = x.shape[1] / sampling_freq
-    adj_time = 0
-    while (adj_time < tot_length_meg):
-        start_stop_seconds = np.array([adj_time, adj_time + duration])
-        start_sample, stop_sample = (start_stop_seconds * sampling_freq).astype(int)
-        raw_selection = raw[:, start_sample:stop_sample][0]
-        raw_selection = raw_selection * shift_value
-        if (raw_selection.shape[1] < (stop_sample - start_sample)):               
-            break
-        data_meg_chunks.append(raw_selection)
-        adj_time = adj_time + duration
-    data_tensor = torch.tensor(data_meg_chunks)
-    data_meg_chunks.remove
-    return data_tensor
 
 def get_bids_raw(meg_path, subject, session, task):
     bids_path = mne_bids.BIDSPath(
@@ -43,19 +25,22 @@ def get_bids_raw(meg_path, subject, session, task):
     )
     try:
         raw = mne_bids.read_raw_bids(bids_path)
+        raw.load_data().filter(0.5, 30.0, n_jobs=1) 
+        raw = raw.pick_types(
+            meg=True, misc=False, eeg=False, eog=False, ecg=False
+        )
     except FileNotFoundError:
         print("missing", subject, session, task)
         pass
-    raw = raw.pick_types(
-        meg=True, misc=False, eeg=False, eog=False, ecg=False
-    )
     return raw
 
-# TODO: applicare lo shift value
+
 def get_meg_from_raw_epochs(epochs):
     data_meg = epochs.get_data()
+    data_meg = data_meg * shift_value
     tensor_meg = torch.tensor(data_meg)
     return tensor_meg
+
 
 def get_epochs(raw, sound_id):
     meta = list()
@@ -87,6 +72,7 @@ def get_epochs(raw, sound_id):
     epochs.apply_baseline()
     return epochs
 
+
 def get_meg_spectrogram(data_tensor):
     data_np = data_tensor.numpy()
     n_frames = (1 + (data_np.shape[2] - n_fft) // hop_length) + n_fft // hop_length
@@ -94,26 +80,10 @@ def get_meg_spectrogram(data_tensor):
     for i in range(data_np.shape[0]): 
         for j in range(data_np.shape[1]): 
             d = librosa.stft(data_np[i, j], n_fft=n_fft, hop_length=hop_length)
-            # spectrograms[i, j] = librosa.amplitude_to_db(np.abs(d), ref=np.max)
-            spectrograms[i, j] = d
+            spectrograms[i, j] = librosa.amplitude_to_db(np.abs(d), ref=np.max)
     spectrograms_tensor = torch.from_numpy(spectrograms)
     return spectrograms_tensor
 
-# maybe to remove
-def get_audio_spectrogram_toRm(audio_path, tot_length_audio, data_audio_chunks):
-    adj_time = 0
-    n_frames = (1 + (sampling_audio * duration - n_fft) // hop_length) + n_fft // hop_length
-    while (adj_time < tot_length_audio):
-        start = adj_time
-        y, sr = librosa.load(audio_path, sr=sampling_audio, offset=start, duration=duration)
-        y_db = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
-        if (y_db.shape[1] < n_frames):     
-            break
-        data_audio_chunks.append(y_db)
-        adj_time = start + duration
-    audio_tensor = torch.tensor(data_audio_chunks)
-    data_audio_chunks.remove
-    return audio_tensor
 
 def get_audio_spectrogram(audio_path, epochs):
     data_audio_chunks = []
@@ -130,5 +100,23 @@ def get_audio_spectrogram(audio_path, epochs):
         data_audio_chunks.append(y_db)
     audio_tensor = torch.tensor(data_audio_chunks)
     return audio_tensor
+
+
+def plot_spectrogram(encode_spect, sr, sample, channel):
+    plt.figure(figsize=(10, 4))
+    if (len(encode_spect.shape) == 4):
+        librosa.display.specshow(encode_spect[sample][channel].numpy(), 
+                                 sr=sr, n_fft=n_fft, hop_length=hop_length, 
+                                 x_axis='time', y_axis='log')
+        plt.title('MEG Spectrogram')
+    else:
+        librosa.display.specshow(encode_spect[sample].numpy(), 
+                                 sr=sr, n_fft=n_fft, hop_length=hop_length, 
+                                 x_axis='time', y_axis='log')
+        plt.title('Audio Spectrogram')
+    plt.colorbar(format='%+2.0f dB')
+    plt.tight_layout()
+    plt.show()
+    
 
 
